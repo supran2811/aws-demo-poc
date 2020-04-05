97c5343fc13ba062d8135a22c15727786822e9cd
Supran Sengupta <supransengupta@gmail.com>
2020-04-06 05:29:55 +0530

    initial code

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  let date_ob = new Date();
+
+  // current date
+  // adjust 0 before single digit date
+  let date = ("0" + date_ob.getDate()).slice(-2);
+
+  // current month
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+  // current year
+  let year = date_ob.getFullYear();
+
+  // current hours
+  let hours = date_ob.getHours();
+
+  // current minutes
+  let minutes = date_ob.getMinutes();
+
+  // current seconds
+  let seconds = date_ob.getSeconds();
+
+  // prints date & time in YYYY-MM-DD HH:MM:SS format
+  const currentDate =
+    year +
+    "-" +
+    month +
+    "-" +
+    date +
+    " " +
+    hours +
+    ":" +
+    minutes +
+    ":" +
+    seconds;
+  res.status(200).send({ currentDate });
+});
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000...");
+});
